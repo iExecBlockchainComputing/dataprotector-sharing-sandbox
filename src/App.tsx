9dcef4d5c98a5b6933223437956fb25a251e6c78
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-05-03 14:53:54 +0200

    Change download logic + add getResultFromCompletedTask

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,15 @@ function App() {
   const [setProtectedDataToRentingSuccess, setSetProtectedDataToRentingSuccess] = useState(false);
   //For RentProtectedData
   const [isLoadingRentProtectedData, setIsLoadingRentProtectedData] = useState(false);
-  const [rentProtectedDataSuccess, setRentProtectedDataSuccess] = useState(false);
   const [isLoadingConsumeProtectedData, setIsLoadingConsumeProtectedData] = useState(false);
+  const [rentProtectedDataSuccess, setRentProtectedDataSuccess] = useState(false);
   //For ConsumeProtectedData
   const [consumeProtectedDataSuccess, setConsumeProtectedDataSuccess] = useState(false);
   const [content, setContent] = useState('');
+  //For GetResultFromCompletedTask
+  const [taskId, setTaskId] = useState('');
+  const [isLoadingGetResultFromCompletedTask, setIsLoadingGetResultFromCompletedTask] = useState(false);
+  const [getResultFromCompletedTaskSuccess, setResultFromCompletedTaskSuccess] = useState(false);
 
   const protectData = async () => {
     try {
@@ -133,6 +137,9 @@ function App() {
       const consumeProtectedDataResult = await iExecDataProtectorClient.sharing.consumeProtectedData({
         protectedData: protectedDataAddressInput,
         app: '0xF248000F0E99e9203FdBE509019f008F9c169705', //An iDapp from the AppWhitelist Managed by iExec Team
+        onStatusUpdate: (status) => {
+          console.log('[consumeProtectedData] status', status);
+        },
       });
       console.log('consumeProtectedDataResult: ', consumeProtectedDataResult);
 
@@ -141,6 +148,27 @@ function App() {
       setConsumeProtectedDataSuccess(true); // show success icon
     } catch (e) {
       setIsLoadingConsumeProtectedData(false); // hide loader
+      console.log(e);
+    }
+  };
+
+  const getResultFromCompletedTask = async () => {
+    try {
+      setResultFromCompletedTaskSuccess(false);
+      setIsLoadingGetResultFromCompletedTask(true); // Show loader
+      const taskResult = await iExecDataProtectorClient.sharing.getResultFromCompletedTask({
+        taskId,
+        onStatusUpdate: (status) => {
+          console.log('[getResultFromCompletedTask] status', status);
+        },
+      });
+      console.log('getResultFromCompletedTask: ', taskResult);
+
+      setContent(taskResult.contentAsObjectURL);
+      setIsLoadingGetResultFromCompletedTask(false); // hide loader
+      setResultFromCompletedTaskSuccess(true); // show success icon
+    } catch (e) {
+      setIsLoadingGetResultFromCompletedTask(false); // hide loader
       console.log(e);
     }
   };
@@ -157,194 +185,220 @@ function App() {
     setProtectedDataPriceInput(event.target.value);
   };
 
+  const handleTaskIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskId(event.target.value);
+  };
+
+  function download() {
+    const element = document.createElement('a');
+    element.setAttribute('href', content);
+    element.setAttribute('download', 'ProtectedDataFile.txt');
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
   return (
     <>
       <div>
+        <h2>Create Test Protected Data</h2>
+        {isLoadingProtectData ? (
+          <img src={loader} alt="loading" height="30px" />
+        ) : (
+          <button onClick={protectData}>Create Test Protected Data</button>
+        )}
+        {protectDataSuccess ? (
+          <div>
+            <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+              <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
+              Successful creation
+            </label>
+          </div>
+        ) : (
+          <></>
+        )}
+        <hr />
+      </div>
+
+      <div>
+        <h2>Create Collection</h2>
+        {isLoadingCreateCollection ? (
+          <img src={loader} alt="loading" height="30px" />
+        ) : (
+          <button onClick={createCollection}>Create Collection</button>
+        )}
+        {createCollectionSuccess ? (
+          <div>
+            <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+              <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
+              Successful creation
+            </label>
+          </div>
+        ) : (
+          <></>
+        )}
+        <hr />
+      </div>
+
+      <div>
+        <h2>Add Protected Data To Collection</h2>
         <div>
-          <h2>Create Test Protected Data</h2>
+          <label>
+            Protected Data Address:{' '}
+            <input
+              name="Protected Data Address"
+              value={protectedData.address}
+              style={{ width: '350px' }}
+              onChange={handleProtectedDataAddressChange}
+            />
+          </label>
         </div>
         <div>
-          {isLoadingProtectData ? (
-            <img src={loader} alt="loading" height="30px" />
-          ) : (
-            <button onClick={protectData}>Create Test Protected Data</button>
-          )}
-          {protectDataSuccess ? (
-            <div>
-              <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
-                <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
-                Successful creation
-              </label>
-            </div>
-          ) : (
-            <></>
-          )}
+          <label>
+            Collection Id:{' '}
+            <input name="Collection Id" value={collection.collectionId} onChange={handleCollectionIdChange} />
+          </label>
         </div>
+        {isLoadingAddProtectedDataToCollection ? (
+          <img src={loader} alt="loading" height="30px" />
+        ) : (
+          <button onClick={addToCollection}>Add Protected Data To Collection</button>
+        )}
+        {addProtectedDataToCollectionSuccess ? (
+          <div>
+            <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+              <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
+              Protected Data Added to Collection
+            </label>
+          </div>
+        ) : (
+          <></>
+        )}
         <hr />
       </div>
 
       <div>
         <div>
-          <h2>Create Collection</h2>
+          <h2>Set Protected Data To Renting</h2>
+          <label>
+            Protected Data Address:{' '}
+            <input
+              name="Protected Data Address"
+              value={protectedData.address}
+              style={{ width: '350px' }}
+              onChange={handleProtectedDataAddressChange}
+            />
+          </label>
         </div>
         <div>
-          {isLoadingCreateCollection ? (
-            <img src={loader} alt="loading" height="30px" />
-          ) : (
-            <button onClick={createCollection}>Create Collection</button>
-          )}
-          {createCollectionSuccess ? (
-            <div>
-              <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
-                <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
-                Successful creation
-              </label>
-            </div>
-          ) : (
-            <></>
-          )}
+          <label>
+            Price of Protected Data:{' '}
+            <input name="Protected Data Price" defaultValue="0" onChange={handleProtectedDataPriceChange} />
+          </label>
         </div>
+        {isLoadingSetProtectedDataToRenting ? (
+          <img src={loader} alt="loading" height="30px" />
+        ) : (
+          <button onClick={setProtectedDataToRenting}>Set Protected Data to Renting</button>
+        )}
+        {setProtectedDataToRentingSuccess ? (
+          <div>
+            <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+              <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
+              Protected Data Set to Renting
+            </label>
+          </div>
+        ) : (
+          <></>
+        )}
         <hr />
       </div>
 
       <div>
+        <h2>Rent Protected Data</h2>
         <div>
-          <div>
-            <h2>Add Protected Data To Collection</h2>
-          </div>
-          <div>
-            <label>
-              Protected Data Address:
-              <input
-                name="Protected Data Address"
-                value={protectedData.address}
-                onChange={handleProtectedDataAddressChange}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Collection Id:
-              <input name="Collection Id" value={collection.collectionId} onChange={handleCollectionIdChange} />
-            </label>
-          </div>
-          {isLoadingAddProtectedDataToCollection ? (
-            <img src={loader} alt="loading" height="30px" />
-          ) : (
-            <button onClick={addToCollection}>Add Protected Data To Collection</button>
-          )}
-          {addProtectedDataToCollectionSuccess ? (
-            <div>
-              <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
-                <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
-                Protected Data Added to Collection
-              </label>
-            </div>
-          ) : (
-            <></>
-          )}
+          <label>
+            Protected Data Address:{' '}
+            <input
+              name="Protected Data Address"
+              value={protectedData.address}
+              style={{ width: '350px' }}
+              onChange={handleProtectedDataAddressChange}
+            />
+          </label>
         </div>
+        {isLoadingRentProtectedData ? (
+          <img src={loader} alt="loading" height="30px" />
+        ) : (
+          <button onClick={rentProtectedData}>Rent Protected Data</button>
+        )}
+        {rentProtectedDataSuccess ? (
+          <div>
+            <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+              <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
+              You are Now Renting The Protected Data
+            </label>
+          </div>
+        ) : (
+          <></>
+        )}
         <hr />
       </div>
 
       <div>
-        <div>
-          <div>
-            <h2>Set Protected Data To Renting</h2>
-          </div>
-          <div>
-            <label>
-              Protected Data Address:
-              <input
-                name="Protected Data Address"
-                value={protectedData.address}
-                onChange={handleProtectedDataAddressChange}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Price of Protected Data:
-              <input name="Protected Data Price" defaultValue="0" onChange={handleProtectedDataPriceChange} />
-            </label>
-          </div>
-          {isLoadingSetProtectedDataToRenting ? (
-            <img src={loader} alt="loading" height="30px" />
-          ) : (
-            <button onClick={setProtectedDataToRenting}>Set Protected Data to Renting</button>
-          )}
-          {setProtectedDataToRentingSuccess ? (
-            <div>
-              <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
-                <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
-                Protected Data Set to Renting
-              </label>
-            </div>
-          ) : (
-            <></>
-          )}
-        </div>
-        <hr />
+        <h2>Consume Your Own Protected Data</h2>
+        {isLoadingConsumeProtectedData ? (
+          <img src={loader} alt="loading" height="30px" />
+        ) : (
+          <button onClick={consumeProtectedData}>Consume Protected Data</button>
+        )}
+        {consumeProtectedDataSuccess && (
+          <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+            <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
+            You can now visualize the rented Protected Data:{' '}
+            <button
+              type="button"
+              onClick={() => {
+                download();
+              }}
+            >
+              Download File
+            </button>
+          </label>
+        )}
       </div>
 
       <div>
+        <h3>Or if you have a completed Task ID</h3>
         <div>
-          <div>
-            <h2>Rent Protected Data</h2>
-          </div>
-          <div>
-            <label>
-              Protected Data Address:
-              <input
-                name="Protected Data Address"
-                value={protectedData.address}
-                onChange={handleProtectedDataAddressChange}
-              />
-            </label>
-          </div>
-          {isLoadingRentProtectedData ? (
-            <img src={loader} alt="loading" height="30px" />
-          ) : (
-            <button onClick={rentProtectedData}>Rent Protected Data</button>
-          )}
-          {rentProtectedDataSuccess ? (
-            <div>
-              <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
-                <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
-                You are Now Renting The Protected Data
-              </label>
-            </div>
-          ) : (
-            <></>
-          )}
+          <label>
+            Completed Task ID:{' '}
+            <input name="Completed Task ID" value={taskId} style={{ width: '500px' }} onChange={handleTaskIdChange} />
+          </label>
         </div>
-        <hr />
-      </div>
-
-      <div>
-        <div>
-          <div>
-            <h2>Consume Your Own Protected Data</h2>
-          </div>
-          {isLoadingConsumeProtectedData ? (
-            <img src={loader} alt="loading" height="30px" />
-          ) : (
-            <button onClick={consumeProtectedData}>Consume Protected Data</button>
-          )}
-          {consumeProtectedDataSuccess ? (
-            <div>
-              <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
-                <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
-                You can now visualize the rented Protected Data :
-                <a href={content} download="ProtectedDataFile.txt">
-                  Download File
-                </a>
-              </label>
-            </div>
-          ) : (
-            <></>
-          )}
-        </div>
+        {isLoadingGetResultFromCompletedTask ? (
+          <img src={loader} alt="loading" height="30px" />
+        ) : (
+          <button onClick={getResultFromCompletedTask}>Get Result From Completed Task</button>
+        )}
+        {getResultFromCompletedTaskSuccess && (
+          <label style={{ display: 'inline-block', verticalAlign: 'middle' }}>
+            <img src={successIcon} alt="success" height="30px" style={{ verticalAlign: 'middle' }} />
+            You can now visualize the rented Protected Data:{' '}
+            <button
+              type="button"
+              onClick={() => {
+                download();
+              }}
+            >
+              Download File
+            </button>
+          </label>
+        )}
       </div>
     </>
   );
